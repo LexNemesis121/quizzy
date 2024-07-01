dc106138dd5face16f876dfab1e6341658b2d82f
Alexandru Verdes <alexandru.verdes@esolutions.ro>
2024-07-01 18:25:00 +0300

    temp fix highlight

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/default.css';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 
 const CodeBlock = ({
 	children,
@@ -9,23 +9,14 @@ const CodeBlock = ({
 	children: React.ReactNode;
 	language?: string;
 }) => {
-	const codeRef = useRef<HTMLElement>(null);
-
 	useEffect(() => {
-		if (codeRef.current) {
-			if (codeRef.current.dataset.highlighted) {
-				codeRef.current.innerHTML = codeRef.current.textContent || '';
-			}
-			hljs.highlightElement(codeRef.current);
-			codeRef.current.dataset.highlighted = 'yes';
-		}
-	}, [children]);
+		const nodes = document.querySelectorAll('pre code');
+		nodes.forEach((node) => hljs.highlightBlock(node as HTMLElement));
+	}, []);
 
 	return (
 		<pre>
-			<code ref={codeRef} className={`language-${language}`}>
-				{children}
-			</code>
+			<code className={`language-${language}`}>{children}</code>
 		</pre>
 	);
 };
